f1d93841c239fd150be3983173742bf769a08233
ikerduque <iker_duque@euneiz.com>
2025-12-01 20:41:09 +0100

    Historia + Controles + Creditos

--- Assets/Scripts/PhotoTarget.cs
+++ Assets/Scripts/PhotoTarget.cs
@@ -12,4 +12,17 @@ public class PhotoTarget : ScriptableObject
     
     [Header("Zoom de la Cámara")]
     public float fieldOfView = 60f; // El FOV con el que se tomó la referencia.
+        
+   
+   
+        
+    // --- NUEVO: LA HISTORIA ---
+    [Header("Narrativa")]
+    public string missionName; // Ej: "La Vieja Torre"
+        
+    [TextArea(5, 10)] // Esto crea una caja grande en el inspector para escribir cómodo
+    public string photoStory; // Ej: "Esta torre fue construida en 1890..."
 }
+    
+    
+   
